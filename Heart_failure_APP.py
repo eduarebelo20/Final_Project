4a5e1fe3a785ddbc0fe05f53f68c788b9d5b0877
eduarebelo20 <eduarebelo20@gmail.com>
2021-05-06 17:15:57 +0100

    Final Commit

--- Heart_failure_APP.py
+++ Heart_failure_APP.py
@@ -1,5 +1,195 @@
-import os
-os.system('clear')
+from tkinter import Label, Tk, Button, Entry
+import pickle
+import xgboost as xgb
+import pandas as pd
 
+
+root = Tk()
+root.title('Heart Failure Prediction')
+
+root.iconbitmap('heart.ico')
+
+label1 = Label(root, text = 'Enter the Age of the patient:', justify = 'left')
+label1.pack()
+
+a = Entry(root, width = 50, borderwidth = 3)
+a.pack()
+
+
+label2 = Label(root, text = 'Enter 1 if the patient has a decrease of hemoglobin or 0 otherwise:', justify = 'left')
+label2.pack()
+
+b = Entry(root, width = 50, borderwidth = 3)
+b.pack()
+
+
+label3 = Label(root, text = 'Enter the level of the CPK enzyme in the blood (mcg/L):', justify = 'left')
+label3.pack()
+
+c = Entry(root, width = 50, borderwidth = 3)
+c.pack()
+
+label4 = Label(root, text = 'Enter 1 if the patient has diabetes or 0 otherwise:', justify = 'left')
+label4.pack()
+
+d = Entry(root, width = 50, borderwidth = 3)
+d.pack()
+
+
+label5 = Label(root, text = 'Percentage of blood leaving the heart at each contraction (%):', justify = 'left')
+label5.pack()
+
+e = Entry(root, width = 50, borderwidth = 3)
+e.pack()
+
+
+label6 = Label(root, text = 'Enter 1 if the patient has hypertension or 0 otherwise::', justify = 'left')
+label6.pack()
+
+f = Entry(root, width = 50, borderwidth = 3)
+f.pack()
+
+
+label7 = Label(root, text = 'Enter the level of platelets in the blood (kiloplatelets/mL):', justify = 'left')
+label7.pack()
+
+g = Entry(root, width = 50, borderwidth = 3, justify = 'left')
+g.pack()
+
+
+label8 = Label(root, text = 'Enter the level of serum creatinine in the blood (mg/dL):', justify = 'left')
+label8.pack()
+
+h = Entry(root, width = 50, borderwidth = 3)
+h.pack()
+
+
+label9 = Label(root, text = 'Enter the level of serum sodium in the blood (mEq/L):', justify = 'left')
+label9.pack()
+
+i = Entry(root, width = 50, borderwidth = 3)
+i.pack()
+
+
+label10 = Label(root, text = 'Enter 1 if the patient is a man or 0 if it is a woman:', justify = 'left')
+label10.pack()
+
+j = Entry(root, width = 50, borderwidth = 3)
+j.pack()
+
+
+label11 = Label(root, text = 'Enter 1 if the patient is a smoker or 0 otherwise:', justify = 'left')
+label11.pack()
+
+k = Entry(root, width = 50, borderwidth = 3)
+k.pack()
+
+
+label12 = Label(root, text = 'Enter the number of days since the patient has had heart failure:', justify = 'left')
+label12.pack()
+
+l = Entry(root, width = 50, borderwidth = 3)
+l.pack()
+
+
+label13 = Label(root, text = 'Name of the patient:', justify = 'left')
+label13.pack()
+
+m = Entry(root, width = 50, borderwidth = 3)
+m.pack()
+
+
+label14 = Label(root, text = 'ID number of the patient:', justify = 'left')
+label14.pack()
+
+n = Entry(root, width = 50, borderwidth = 3)
+n.pack()
+
+
+label15 = Label(root, text = 'Date (dd/mm/yy):', justify = 'left')
+label15.pack()
+
+o = Entry(root, width = 50, borderwidth = 3)
+o.pack()
+
+label16 = Label(root, text = '', justify = 'left')
+label16.pack()
+
+label17 = Label(root, text = '', justify = 'left')
+label17.pack()
+
+def predict_outcome():
+    
+    try:
+        ag = float(a.get()),
+        bg = float(b.get()),
+        cg = float(c.get()),
+        dg = float(d.get()),
+        eg = float(e.get()),
+        fg = float(f.get()),
+        gg = float(g.get()),
+        hg = float(h.get()),
+        ig = float(i.get()),
+        jg = float(j.get()),
+        kg = float(k.get()),
+        lg = float(l.get()),
+        mg = str(m.get()),
+        ng = int(n.get()),
+        og = str(o.get())
+        
+    except ValueError:
+        label16.config(text = '''Review the answers in each entry! Make sure that the numbers
+with decimal plates have a. (dot) instead of a , (comma).
+Also make sure that there aren't any entries with letters/numbers mixed
+letters/special characters mixed, numbers/special characters mixed, except for the Date.''')
+        
+    new_data_point = [ag, bg, cg, dg, eg, fg, gg, hg, ig, jg, kg, lg, mg, ng, og]
+    
+    df = pd.DataFrame([], columns = ['age', 'anaemia', 'creatinine_phosphokinase', 'diabetes',
+                                     'ejection_fraction', 'high_blood_pressure', 'platelets',
+                                     'serum_creatinine', 'serum_sodium', 'sex', 'smoking', 'time',
+                                     'patient_name', 'Id_number_patient', 'Date'])
+    
+    z = 0
+    for column in df.columns:
+        df[column] = new_data_point[z]
+        z += 1
+      
+    filename = 'xgboost_heart_failure_model.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+    result = loaded_model.predict(xgb.DMatrix(df[['age', 'anaemia', 'creatinine_phosphokinase',
+                                                  'diabetes', 'ejection_fraction',
+                                                  'high_blood_pressure', 'platelets',
+                                                  'serum_creatinine', 'serum_sodium', 'sex',
+                                                  'smoking', 'time']]))
+    if result == 1.0:
+        label17.config(text = 'The patient is likely to have heart failure!')
+    else:
+        label17.config(text = 'The patient is unlikely to have heart failure!')
+        
+def delete():
+    a.delete(0, 'end')
+    b.delete(0, 'end')
+    c.delete(0, 'end')
+    d.delete(0, 'end')
+    e.delete(0, 'end')
+    f.delete(0, 'end')
+    g.delete(0, 'end')
+    h.delete(0, 'end')
+    i.delete(0, 'end')
+    j.delete(0, 'end')
+    k.delete(0, 'end')
+    l.delete(0, 'end')
+    m.delete(0, 'end')
+    n.delete(0, 'end')
+    o.delete(0, 'end')
+    
+SubmitButton = Button(root, text = 'Submit Values', command = predict_outcome)
+SubmitButton.pack()
+
+DeleteEntries = Button(root, text = "Delete all entries", command = delete)
+DeleteEntries.pack()
+
+root.mainloop()
 
 
--- XGBTest.py
+++ XGBTest.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 
 heart_failure = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 
@@ -25,8 +24,8 @@ for column in list(heart_failure_descriptive_stats.columns):
     print('lower:', len(list_lower_out))
     print('higher:', len(list_higher_out))
     
-##The outliers are relevant to describe specific scenarios for the pacients. Either respresenting 'certain'
-##survival or 'certain' death
+##The outliers are relevant to describe specific scenarios for the pacients. Either respresenting
+##'certain' survival or 'certain' death
 
 heart_failure.isna().sum()
 
@@ -45,7 +44,7 @@ engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                                pw="Hisbenfica97",
                                db="final_project"))
 
-heart_failure.to_sql("heart_failure", con = engine, if_exists = "append")
+heart_failure.to_sql("heart_failure", con = engine, if_exists = "fail")
 
 ##Initialize a connector to query the database that is on SQL
 
@@ -63,17 +62,38 @@ mycursor.execute("SELECT age, anaemia, creatinine_phosphokinase, diabetes, eject
 x = mycursor.fetchall()
 
 x = pd.DataFrame(x)
+x.columns = ['age', 'anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction',
+             'high_blood_pressure', 'platelets', 'serum_creatinine', 'serum_sodium', 'sex',
+             'smoking', 'time']
 
 mycursor.execute("SELECT DEATH_EVENT FROM final_project.heart_failure")
 y = mycursor.fetchall()
 
 y = pd.DataFrame(y)
+y.columns = ['DEATH_EVENT']
 
 ##Split the data
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(x, y)
+
+##Now let's oversample the data to deal with class imbalance
+from sklearn.utils import resample
+
+train = pd.concat([X_train, y_train],axis=1)
+
+train.head()
+
+no_death  = train[train['DEATH_EVENT']==0]
+yes_death = train[train['DEATH_EVENT']==1]
+
+yes_death_oversampled = resample(yes_death, replace=True, n_samples = len(no_death), random_state=0)
+
+train_oversampled = pd.concat([no_death, yes_death_oversampled])
+
+y_train_over = train_oversampled['DEATH_EVENT'].copy()
+X_train_over = train_oversampled.drop('DEATH_EVENT',axis = 1).copy()
 
 ##Grid Search to get the best parameters for the XGBoosting
 
@@ -83,7 +103,7 @@ from sklearn.model_selection import GridSearchCV
 max_depth = [4,5,6,7,8,9,10]
 min_child_weight = [1,2,3,4,5,6]
 gamma = [0.1, 0.2, 0.3, 0.4]
-learning_rate = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+learning_rate = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 subsample = [0.5, 0.6, 0.7, 0.8]
 
 grid = {'booster': ['gbtree']
@@ -96,12 +116,87 @@ grid = {'booster': ['gbtree']
     ,'verbosity': [3]
 }
 
-booster = xgb.XGBClassifier('binary:hinge')
+booster = xgb.XGBClassifier(grid)
 
 grid_search = GridSearchCV(estimator = booster, param_grid = grid,  cv = 3)
 
-grid_search.fit(X_train, y_train)
+grid_search.fit(X_train_over, y_train_over)
 
 print(grid_search.best_params_)
 print(grid_search.best_score_)
-##{'booster': 'gbtree', 'gamma': 0.1, 'learning_rate': 0.2, 'max_depth': 5, 'min_child_weight': 4, 'objective': 'binary:hinge', 'subsample': 0.7, 'verbosity': 3}
+
+##Create the model in order to calculate accuracy and check the number of false negatives
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+##for i in range(100):
+    
+param = grid_search.best_params_
+    
+    
+d_train = xgb.DMatrix(X_train_over, y_train_over)
+d_test = xgb.DMatrix(X_test, y_test)
+    
+model = xgb.train(param, d_train, num_boost_round = 35)
+    
+y_pred = model.predict(d_test)
+    
+print(roc_auc_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+    
+##Auxiliary piece of code used with the for cicle commented in line 131 to determine the best
+#parameter num_boost_rouund
+
+'''list1 = []
+list2 = []
+
+if roc_auc_score(y_test, y_pred) > 0.80:
+    list1.append((roc_auc_score(y_test, y_pred)))
+       list1.append(i)
+       list2.append(confusion_matrix(y_test, y_pred)[0][1])
+       list2.append(i)'''
+
+##KNN to compare results with the XGBoost
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
+for i in range(1, 100):
+    knn = KNeighborsClassifier(i)
+    
+    knn.fit(X_train_over, y_train_over)
+    
+    ##Standardized data
+    
+    scale = StandardScaler().fit(pd.concat([X_train_over, X_test]))
+    
+    X_train_std = scale.transform(X_train_over)
+    X_test_std  = scale.transform(X_test)
+    
+    model_knn = knn.fit(X_train_std, y_train_over)
+    
+    y_pred = model_knn.predict(X_test_std)
+    
+    if roc_auc_score(y_test, y_pred) > 0.75:
+        print(roc_auc_score(y_test, y_pred), i)
+        cm = confusion_matrix(y_test, y_pred)
+        print(cm)
+        
+        
+#Create a file for the model to use with the app
+
+import pickle
+
+# save the model to disk
+filename = 'xgboost_heart_failure_model.sav'
+pickle.dump(model, open(filename, 'wb'))
+ 
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, y_test)
+print(result)
+
+cnx.commit()
+mycursor.close()
+cnx.close() 
